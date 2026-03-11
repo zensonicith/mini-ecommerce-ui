7dd1b69f2ee58b8fa061a0333c8993a555b3e2a7
Binh Ho Vu Thanh <binh19220042018@gmail.com>
2026-03-11 11:53:29 +0700

    feat: add login

---
+++ src/app/login.service.ts
@@ -0,0 +1,42 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { CustomerInfo } from './user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  url = 'http://localhost:5291/api/auth/login';
+  currentCustomer = signal<CustomerInfo | null>(null);
+  isLoggedIn = computed(() => this.currentCustomer() !== null);
+  errorMessage = signal<string | null>(null);
+  constructor() {}
+
+  // Call api for login, if success, set currentCustomer and return true, else set errorMessage and return false
+  async login(username: string, password: string) {
+    if (!username || !password) {
+      throw new Error('Username and password are required');
+    }
+    const body = { username, password };
+    const data = await fetch(this.url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    });
+
+    if (data.ok) {
+      const responseJson = await data.json();
+      const customer: CustomerInfo = responseJson.customer;
+      this.currentCustomer.set(customer);
+      this.errorMessage.set(null);
+      return true;
+    }
+    else{
+      const errorData = await data.json();
+      this.errorMessage.set(errorData.message || 'Login failed');
+      return false;
+    }
+  }
+  logout() {
+    this.currentCustomer.set(null);
+  }
+}
---
+++ src/app/user.ts
@@ -0,0 +1,7 @@
+export interface CustomerInfo {
+    id: number;
+    name: string;
+    address: string;
+    userName: string;
+    city: string;
+}
